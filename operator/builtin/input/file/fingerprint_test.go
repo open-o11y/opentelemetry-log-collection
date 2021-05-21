42931ada82ef26fde2c6e62e6fa9495997bee2cd
Dan Jaglowski <dan.jaglowski@bluemedora.com>
2021-05-21 13:41:29 -0400

    Close files in fingerprint test

--- operator/builtin/input/file/fingerprint_test.go
+++ operator/builtin/input/file/fingerprint_test.go
@@ -245,6 +245,8 @@ func TestFingerprintStartsWith_FromFile(t *testing.T) {
 
 	fullFile, err := ioutil.TempFile(tempDir, "")
 	require.NoError(t, err)
+	defer fullFile.Close()
+
 	_, err = fullFile.Write(content)
 	require.NoError(t, err)
 
@@ -253,14 +255,15 @@ func TestFingerprintStartsWith_FromFile(t *testing.T) {
 
 	partialFile, err := ioutil.TempFile(tempDir, "")
 	require.NoError(t, err)
+	defer partialFile.Close()
 
 	// Write the first byte before comparing, since empty files will never match
 	_, err = partialFile.Write(content[:1])
 	require.NoError(t, err)
 	content = content[1:]
 
-	// Write one byte at a time and validate that updated
-	// full fingerprint still starts with partial
+	// Write one byte at a time and validate that
+	// full fingerprint still starts with updated partial
 	for i := range content {
 		_, err = partialFile.Write(content[i:i])
 		require.NoError(t, err)
@@ -268,9 +271,7 @@ func TestFingerprintStartsWith_FromFile(t *testing.T) {
 		pff, err := operator.NewFingerprint(partialFile)
 		require.NoError(t, err)
 
-		match := fff.StartsWith(pff)
-
-		require.True(t, match)
+		require.True(t, fff.StartsWith(pff))
 	}
 }
 
